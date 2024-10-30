e3aa3c70794541ca94c50246f6f5bc7c3034d132
nhannm-vn <nhanmin911@gmail.com>
2024-10-31 00:07:28 +0700

    update 31-10-2024

--- shoppingCardBE/src/constants/enums.ts
+++ shoppingCardBE/src/constants/enums.ts
@@ -10,3 +10,10 @@ export enum USER_ROLE {
   Staff, //1
   User //2
 }
+
+export enum TokenType {
+  AccessToken, //0
+  RefreshToken, //1
+  ForgotPasswordToken, //2
+  EmailVerificationToken //3
+}
--- shoppingCardBE/src/services/users.services.ts
+++ shoppingCardBE/src/services/users.services.ts
@@ -5,8 +5,34 @@ import User from '~/models/schemas/User.schema'
 import databaseServices from './database.services'
 import { RegisterReqBody } from '~/models/requests/users.request'
 import { hashPassword } from '~/utils/crypto'
+import { signToken } from '~/utils/jwt'
+import { TokenType } from '~/constants/enums'
 
 class UserServices {
+  //_Vì mình viết 2 chữ ký thì phải qua đây
+  //vì đây là vì đây sẽ là hàm sẽ ký ra chữ ký khi đã tạo thành công tài khoản nghĩa là khi dữ liệu đã đầy đủ và chuẩn
+  //mặc khác khi ký xong sẽ có nhu cầu lu trên db và trả ra user nên ở đây
+
+  //_Khi em gọi hàm này, đưa cho anh user_id để định danh thì anh sẽ đưa lại cho mã token
+  private signAccessToken = (user_id: string) => {
+    //**Lưu ý: signToken là một promise nhưng mình sẽ k await vì nếu mình await
+    //thì nó sẽ phải đợi. nên mình sẽ return ra luôn
+    //nên dẫn tới khi nào mình xài hàm thì mới cần await
+    return signToken({
+      //_payload sẽ chứa user_id để định danh để biết nó là account nào, đồng thời chứa coi type của nó chức năng dùng để làm gì
+      payload: { user_id, token_Type: TokenType.AccessToken },
+      //_option sẽ truyền cho nó thời gian hết hạn của nó, không để arthims thì mặc đinh là sh256
+      options: { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_IN }
+    })
+  }
+
+  private signRefreshToken = (user_id: string) => {
+    return signToken({
+      payload: { user_id, token_Type: TokenType.RefreshToken },
+      options: { expiresIn: process.env.REFRESH_TOKEN_EXPIRE_IN }
+    })
+  }
+
   //_Vì mình muốn là trước khi tạo account thì check coi có trùng email trên hệ thống chưa
   //nên sẽ viết hàm tại đây
   async checkEmailExist(email: string): Promise<boolean> {
@@ -28,8 +54,24 @@ class UserServices {
         password: hashPassword(payload.password)
       })
     )
-    return result
-    //trả ra để có thể thông qua res mà báo cho người dùng
+    //_Lúc này khi đăng ký thành công thì sẽ tạo cho 2 cái mã và trả cho người dùng luôn để người dùng cầm quyền có thể đăng nhập vào liền
+    //mình sẽ đợi 2 thằng này tạo ra mã sẽ tốn time nhưng mình sẽ cho 2 thằng chạy bất dồng bộ cho đỡn tốn time
+    //vậy khi có tốn time thì sẽ tốn time 1 lần thôi
+
+    //_Mình muốn tạo ra 2 cái mã thì cần user_id để định danh tuy nhiên. Mình sẽ không biết
+    //cái mã đó lấy như thế nào. Tuy nhiên may mắn là trong result khi register đăng ký thành công thì có
+    //insertedid tuy nhiên nó là object cần biến về string
+    const user_id = result.insertedId.toString()
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.signAccessToken(user_id),
+      this.signRefreshToken(user_id)
+    ])
+    //==> Khi mà đăng ký thành công thì sẽ đưa luôn 2 cái chữ ký để người ta xài luôn
+    return {
+      accessToken,
+      refreshToken
+    }
   }
 }
 
