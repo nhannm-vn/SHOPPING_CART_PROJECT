03dbee739896f31efe9c475745961ab57589966a
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2024-12-03 14:57:25 +0700

    update 3-12-2024

--- shoppingCardBE/src/constants/regex.ts
+++ shoppingCardBE/src/constants/regex.ts
@@ -1,3 +1,4 @@
 //_module chuyen luu cac regex
 export const REGEX_USERNAME = /^(?![0-9]+$)[A-Za-z0-9_]{4,15}$/
 export const REGEX_PHONE_NUMBER = /^0[0-9]{9,11}$/
+export const REGEX_TEST = /^[aA][bB]$/
